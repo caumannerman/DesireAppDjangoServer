a5110d14dc4de50e798947b39d3b78ea841b40ff
litsynp <nocte55is@gmail.com>
2021-11-06 23:30:13 +0900

    build: set up Django REST Framework

--- core/settings.py
+++ core/settings.py
@@ -37,6 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Third-party apps
+    'rest_framework',  # Django REST Framework
+    'django_filters',  # django-filter
+
+    # Local apps
+
 ]
 
 MIDDLEWARE = [
