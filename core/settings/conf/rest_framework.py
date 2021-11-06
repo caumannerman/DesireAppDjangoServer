ddd06db2e25a96fb4917f86eff607145b811ac7d
litsynp <nocte55is@gmail.com>
2021-11-07 00:57:53 +0900

    feat: set up default permission, pagination and filter

--- core/settings/conf/rest_framework.py
+++ core/settings/conf/rest_framework.py
@@ -1,5 +1,16 @@
 REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
+
+    # Pagination
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 20,
+
+    # Filtering
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
 }
