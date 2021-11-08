affbe6215c434e702084b40d9ec2567b56d49dcf
litsynp <nocte55is@gmail.com>
2021-11-08 16:23:57 +0900

    fix: fix filter error in question API

--- questions/views.py
+++ questions/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import viewsets
+from rest_framework import permissions, viewsets
 from rest_framework.filters import SearchFilter, OrderingFilter
 
 from questions.models import Question
@@ -8,9 +8,10 @@ from questions.serializers import QuestionSerializer
 
 
 class QuestionViewSet(viewsets.ModelViewSet):
-    filter_backends = (DjangoFilterBackend, SearchFilter, OrderingFilter,)
-    search_fields = ()
-    ordering_fields = ('created_on')
-    # filterset_fields = ('user__id')
     queryset = Question.objects.all()
     serializer_class = QuestionSerializer
+    filter_backends = (DjangoFilterBackend, SearchFilter, OrderingFilter,)
+    filter_fields = ['user__id']
+    search_fields = []
+    ordering_fields = ['created_on']
+    permission_classes = [permissions.AllowAny]
