4a0cdebe3b389fdcbe9b639a1e83b5b5d9e42f0b
litsynp <nocte55is@gmail.com>
2021-11-10 00:26:18 +0900

    refactor: organize codes in question app

--- questions/models.py
+++ questions/models.py
@@ -1,8 +1,8 @@
 import uuid
 
-from django.contrib.auth import get_user_model
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
--- questions/views.py
+++ questions/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
 from rest_framework.filters import SearchFilter, OrderingFilter
