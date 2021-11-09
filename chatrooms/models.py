5d2ed85d02fe3b8456d631317154ad28c17cc569
litsynp <nocte55is@gmail.com>
2021-11-10 00:43:32 +0900

    fix: change chat room table structure

--- chatrooms/models.py
+++ chatrooms/models.py
@@ -4,19 +4,19 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 from django.contrib.auth import get_user_model
 
-from questions.models import Question
-
 User = get_user_model()
 
 
 class ChatRoom(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    user = models.ForeignKey(
+    sender = models.ForeignKey(
         User,
+        related_name='sender_chatrooms',
         on_delete=models.CASCADE
     )
-    question = models.OneToOneField(
-        Question,
+    recipient = models.ForeignKey(
+        User,
+        related_name='recipient_chatrooms',
         on_delete=models.CASCADE
     )
     created_on = models.DateTimeField(
@@ -29,4 +29,4 @@ class ChatRoom(models.Model):
     )
 
     def __str__(self):
-        return 'Chatroom for [Question title] "{}" created by [User email] "{}"'.format(self.question.title, self.user.email)
+        return 'Chatroom between "{}" and "{}"'.format(self.sender.email, self.recipient.email)
