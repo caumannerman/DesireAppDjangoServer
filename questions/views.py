b90ec53cd6f99c6c65c5d7ac8ac538d075445f70
litsynp <nocte55is@gmail.com>
2021-11-12 22:25:52 +0900

    feat: add filter by category name for question API

--- questions/views.py
+++ questions/views.py
@@ -1,4 +1,6 @@
+from django_filters.filters import BaseInFilter
 from django_filters.rest_framework import DjangoFilterBackend
+from django_filters import FilterSet
 from rest_framework import viewsets
 from rest_framework.filters import SearchFilter, OrderingFilter
 
@@ -7,11 +9,20 @@ from questions.serializers import QuestionSerializer
 from core.permissions import OwnerPermission
 
 
+class QuestionFilter(FilterSet):
+    categories__name__in = BaseInFilter(
+        field_name='categories__name', lookup_expr='in')
+
+    class Meta:
+        model = Question
+        fields = ['user__id', 'categories__name__in', ]
+
+
 class QuestionViewSet(viewsets.ModelViewSet):
     queryset = Question.objects.all()
     serializer_class = QuestionSerializer
     filter_backends = (DjangoFilterBackend, SearchFilter, OrderingFilter,)
-    filter_fields = ['user__id']
+    filterset_class = QuestionFilter
     search_fields = []
-    ordering_fields = ['created_on']
+    ordering_fields = ['created_on', ]
     permission_classes = [OwnerPermission]
