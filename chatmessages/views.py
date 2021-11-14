1a1de097363240f9b848f78d15da8e7d3a951835
litsynp <nocte55is@gmail.com>
2021-11-14 22:11:53 +0900

    feat: add more detail to chat message API

--- chatmessages/views.py
+++ chatmessages/views.py
@@ -3,15 +3,20 @@ from rest_framework import viewsets
 from rest_framework.filters import SearchFilter, OrderingFilter
 
 from chatmessages.models import ChatMessage
-from chatmessages.serializers import ChatMessageSerializer
+from chatmessages.serializers import ChatMessageListRetrieveSerializer, ChatMessageSerializer
 from core.permissions import OwnerPermission
 
 
 class ChatMessageViewSet(viewsets.ModelViewSet):
     queryset = ChatMessage.objects.all()
-    serializer_class = ChatMessageSerializer
     filter_backends = (DjangoFilterBackend, SearchFilter, OrderingFilter,)
     filter_fields = ['user__id', 'chatroom__id']
     search_fields = []
     ordering_fields = ['created_on']
     permission_classes = [OwnerPermission]
+
+    def get_serializer_class(self):
+        if self.action in ['list', 'retrieve']:
+            return ChatMessageListRetrieveSerializer
+        else:
+            return ChatMessageSerializer
