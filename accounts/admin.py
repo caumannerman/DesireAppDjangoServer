68559bd27d1d8712b99e2170d255da82147f3bb4
litsynp <nocte55is@gmail.com>
2021-11-14 22:23:05 +0900

    feat: encrypt password on changing password from Django admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,17 +1,36 @@
 from django.contrib import admin
 from django.contrib.auth import get_user_model
 from django.contrib.sites.models import Site
+from django.contrib.auth.admin import UserAdmin as OrigUserAdmin
+from django.utils.translation import gettext_lazy as _
 from allauth.account.models import EmailAddress
 from allauth.socialaccount.models import SocialAccount, SocialApp, SocialToken
 
 User = get_user_model()
 
 
-class UserAdmin(admin.ModelAdmin):
-    list_display = ('id', 'email', 'first_name', 'last_name',)
+class UserAdmin(OrigUserAdmin):
+    list_display = ('id', 'email', 'acc_type', 'nickname',
+                    'first_name', 'last_name',)
     search_fields = ('email', 'first_name', 'last_name',)
-    list_filter = ('is_active', 'is_superuser', 'is_staff',
+    list_filter = ('acc_type', 'is_active', 'is_superuser', 'is_staff',
                    'date_joined', 'last_login',)
+    ordering = ('email',)
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2'),
+        }),
+    )
+    fieldsets = (
+        (None, {'fields': ('email', 'password',
+         'nickname', 'acc_type', 'profile_image')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name')}),
+        (_('Permissions'), {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
+        }),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
 
 
 admin.site.register(User, UserAdmin)
