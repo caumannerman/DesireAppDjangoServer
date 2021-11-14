9031be4fede01eb0b8b938b0b2f8f26f3c6a4ca0
litsynp <nocte55is@gmail.com>
2021-11-14 22:11:27 +0900

    feat: add more detail to answer API

--- answers/serializers.py
+++ answers/serializers.py
@@ -1,6 +1,15 @@
 from rest_framework import serializers
 
+from accounts.serializers import UserSerializer
 from answers.models import Answer
+
+
+class AnswerListRetrieveSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
+
+    class Meta:
+        model = Answer
+        fields = '__all__'
 
 
 class AnswerSerializer(serializers.ModelSerializer):
--- answers/views.py
+++ answers/views.py
@@ -3,15 +3,20 @@ from rest_framework import viewsets
 from rest_framework.filters import SearchFilter, OrderingFilter
 
 from answers.models import Answer
-from answers.serializers import AnswerSerializer
+from answers.serializers import AnswerListRetrieveSerializer, AnswerSerializer
 from core.permissions import OwnerPermission
 
 
 class AnswerViewSet(viewsets.ModelViewSet):
     queryset = Answer.objects.all()
-    serializer_class = AnswerSerializer
     filter_backends = (DjangoFilterBackend, SearchFilter, OrderingFilter,)
     filter_fields = ['user__id', 'question__id']
     search_fields = []
     ordering_fields = ['created_on']
     permission_classes = [OwnerPermission]
+
+    def get_serializer_class(self):
+        if self.action in ['list', 'retrieve']:
+            return AnswerListRetrieveSerializer
+        else:
+            return AnswerSerializer
