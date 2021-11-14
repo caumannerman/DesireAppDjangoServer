a2b6d7d5c2f812f41e2b4769c9260309a3fac904
litsynp <nocte55is@gmail.com>
2021-11-14 22:11:44 +0900

    chore: remove useless code in chatroom views

--- chatrooms/views.py
+++ chatrooms/views.py
@@ -1,5 +1,5 @@
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import serializers, viewsets
+from rest_framework import viewsets
 from rest_framework.filters import SearchFilter, OrderingFilter
 
 from chatrooms.models import ChatRoom
