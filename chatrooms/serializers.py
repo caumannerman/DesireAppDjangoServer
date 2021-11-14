e642b7a115ce2bdaaf30796567e6e6d92ed7d1ff
litsynp <nocte55is@gmail.com>
2021-11-14 21:49:11 +0900

    feat: add latest chat message to chat room API
    Resolves #3.

--- chatrooms/serializers.py
+++ chatrooms/serializers.py
@@ -1,9 +1,24 @@
 from rest_framework import serializers
 
 from chatrooms.models import ChatRoom
+from chatmessages.models import ChatMessage
+from chatmessages.serializers import ChatMessageSerializer
 
 
 class ChatRoomSerializer(serializers.ModelSerializer):
+    latest_chat_message = serializers.SerializerMethodField()
+
     class Meta:
         model = ChatRoom
-        fields = '__all__'
+        fields = ['id', 'sender', 'recipient',
+                  'latest_chat_message', 'created_on', 'updated_on', ]
+        extra_fields = ['latest_chat_message', ]
+
+    def get_latest_chat_message(self, obj):
+        try:
+            latest_chat_message = ChatMessage.objects.filter(
+                chatroom=obj).order_by('-created_on').first()
+            serializer = ChatMessageSerializer(latest_chat_message)
+            return serializer.data
+        except:
+            return None
